09f298478ed43ed70450dae82c96b27ad2d69e37
Yoshida22 Dev <yoshio_salvador@hotmail.com>
2023-04-13 22:19:32 -0600

    Se agregaron navigations para las screens para tener mejor control de las screens

---
+++ src/screens/Pokedex.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native'
+import React from 'react'
+import Pokemon from './Pokemon'
+
+export default function Pokedex() {
+  return (
+    <SafeAreaView>
+      <Text>Listado de pokemon</Text>
+    </SafeAreaView>
+  )
+}

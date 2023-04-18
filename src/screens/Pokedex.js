4d95691a43a273fa93c0e9224857fb3d037a802f
Yoshida22 Dev <yoshio_salvador@hotmail.com>
2023-04-17 22:58:42 -0600

    Muestra los primeros 20 pokemon de la lista en pokedex con su color de fondo del tipo de pokemon

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,11 +1,45 @@
-import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
-import Pokemon from './Pokemon'
+import { SafeAreaView } from "react-native";
+import React, { useState, useEffect } from "react";
+import Pokemon from "./Pokemon";
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
+import PokemonList from "../components/PokemonList";
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log("Pokemons----->", pokemons);
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      const pokemonsArray = [];
+
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          image: pokemonDetails.sprites.other["official-artwork"].front_shiny,
+        });
+      }
+      console.log(pokemonsArray);
+      //spring interator añade la info de los arrays a  otro array, sin modificar los valores de los arrays que hay dentro
+      setPokemons([...pokemons, ...pokemonsArray]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
-      <Text>Listado de pokemon</Text>
+      <PokemonList pokemons={pokemons} />
     </SafeAreaView>
-  )
+  );
 }

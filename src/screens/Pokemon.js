8e0e22f5ea9d752ce62df40d13bd6d459c5efad2
Yoshida22 Dev <yoshio_salvador@hotmail.com>
2023-04-19 21:26:03 -0600

    Se modificó el icono de regresar

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -4,6 +4,7 @@ import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
 import Stats from "../components/Pokemon/Stats";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 export default function Pokemon(props) {
   const {
@@ -11,6 +12,21 @@ export default function Pokemon(props) {
     route: { params },
   } = props;
   const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color={"#fff"}
+          size={20}
+          style={{ marginLeft: 0 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   //Esta uncion se ejecuta cada que params sea modificado
   useEffect(() => {
@@ -35,8 +51,8 @@ export default function Pokemon(props) {
         image={pokemon.sprites.other["official-artwork"].front_shiny}
         type={pokemon.types[0].type.name}
       />
-      <Type types={pokemon.types}/>
-      <Stats stats={pokemon.stats}/>
+      <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
     </ScrollView>
   );
 }
